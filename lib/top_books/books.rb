0aea5f75285a72bf39e971fca6169ce08772fbbe
Lauren McCoy <lmccoy8911@gmail.com>
2019-06-21 13:43:01 +0000

    Added to Books class, edited the scraper class to more concise code

--- lib/top_books/books.rb
+++ lib/top_books/books.rb
@@ -1,6 +1,22 @@
 require 'pry'
-
 class Books 
+  attr_accessor :author, :title 
   
+   @@authors = []
+   @@titles = [] 
+  
+  def author 
+    @author = author 
+    
+  end
+  
+  def title 
+    @title = title   
+  end 
+  
+  def book 
+        
+  end
+
 
 end 
--- lib/top_books/scraper.rb
+++ lib/top_books/scraper.rb
@@ -1,20 +1,23 @@
 class Scraper 
+  attr_reader :title, :author 
+  
+  def get_page(site)
+    site = "https://www.barnesandnoble.com/b/top-books-of-the-month/_/N-2luc"
+    page = Nokogiri::HTML(open(site))
+  end 
   
   def scraper 
-    
-  
-      site = "https://www.barnesandnoble.com/b/top-books-of-the-month/_/N-2luc"
-
-      titles = Array.new
-      authors = Array.new
-      page = Nokogiri::HTML(open(site))
-
-      author = page.css(".product-shelf-author").collect do |writer|
-            authors << writer.text
+    author = get_page(page).css(".product-shelf-author")
+     
+     
+      author.collect do |writer|
+            @@author << writer.text
       end 
-
-      title = page.css(".product-shelf-title").collect do |book|
-            titles << book.text
+    
+    title = get_page(page).css(".product-shelf-title")
+      
+      title.collect do |book|
+          @@title << book.text
       end 
   end   
     
