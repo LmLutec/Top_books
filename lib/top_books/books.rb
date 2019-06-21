01949cd29bf34d39712bee0d5bac1aed0b24469a
Lauren McCoy <lmccoy8911@gmail.com>
2019-06-21 13:24:05 +0000

    Created a scraper.rb

--- lib/top_books/books.rb
+++ lib/top_books/books.rb
@@ -1,21 +1,6 @@
 require 'pry'
 
 class Books 
-  def books 
   
-      site = "https://www.barnesandnoble.com/b/top-books-of-the-month/_/N-2luc"
-
-      titles = Array.new
-      authors = Array.new
-      page = Nokogiri::HTML(open(site))
-
-      author = page.css(".product-shelf-author").collect do |writer|
-            authors << writer.text
-      end 
-
-      title = page.css(".product-shelf-title").collect do |book|
-            titles << book.text
-      end 
-  end 
 
 end 
