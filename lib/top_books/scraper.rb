07373452070a653271d8a4bd395110b8730ff204
Lauren McCoy <lmccoy8911@gmail.com>
2019-06-25 23:34:08 +0000

    edited scraper class

--- lib/top_books/scraper.rb
+++ lib/top_books/scraper.rb
@@ -8,16 +8,62 @@ class Scraper
   end 
   
   def get_books
-     titles = get_site site.css(".product-shelf-title a").children
+     new_site = get_site
+     site.css(".product-shelf-title a").children
   end 
   
   def create_book_list
-    
+    binding.pry 
+    titles.collect do |book|
+        book = Books.new 
+        book.title = new_site.children
+      end 
+  end 
+  
+  #def scraper 
+    # author = get_page(page).css(".product-shelf-author")
+     
+     
+    #   author.collect do |writer|
+    #         @@author << writer.text
+    #   end 
+    # titles.each_with_index do |book,index|
+    #     book = Books.new 
+    #     book.title = titles 
+    #   end    
+ 
+      #@@all << "#{index + 1}:#{book}" 
+    #title.each.with_index {|book ,index|puts "#{index+=1}:#{book.text.strip}"}
+      # writers = page.css(".product-shelf-author a").children
+    # puts "#{titles[1]} by #{writers[1]}"
+end   
+  
+  def create_book_list
+    binding.pry 
+    titles.collect do |book|
         book = Books.new 
         book.title = titles 
       end 
   end 
+  class Scraper 
   
+  
+  # def get_site 
+  #   site = Nokogiri::HTML(open("https://www.barnesandnoble.com/b/new-releases/_/N-1oyg"))
+  # end 
+  
+  # def get_books
+  #   site = get_site 
+  #       new_site = site.css(".product-shelf-title a")
+  # end 
+  
+  # def create_book_list 
+  #   get_books.collect do |new_site|
+  #       book = Books.new 
+  #       book.title = new_site.children 
+  #       puts book.title
+  #     end 
+  # end
   #def scraper 
     # author = get_page(page).css(".product-shelf-author")
      
