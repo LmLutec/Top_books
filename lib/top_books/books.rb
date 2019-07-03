d0b7483e2d950615cc752eb26b624adcc91a9eb8
Lauren McCoy <lmccoy8911@gmail.com>
2019-07-03 23:33:31 +0000

    created Books.get_input(selection) method

--- lib/top_books/books.rb
+++ lib/top_books/books.rb
@@ -22,28 +22,11 @@ class Books
     end 
   end 
   
+  def get_input(selection) 
+     selection = selection.to_i 
+     selection.between?(1,10)
+          puts @@all[selection]
+  end 
   
 end 
-  # def new_book
-  #   @scraped.collect do |book|
-  #     book = Books.new
-  #   end 
-  # end
   
-  # def booklist 
-  #   @booklist.each_with_index do |book,index|
-  #     puts "#{index + 1}:#{book}"
-  #     end   
-  # end 
-
-  # def get_input(input)
-  #   puts "Select the Title number to view more about that New Release:" 
-  #   input = gets.strip
-  #   new_input = input+=1 
-  #   @new_input = new_input 
-  # end
-  
-
-  # def match 
-  #   puts @booklist[@new_input]
-  # end 
--- lib/top_books/scraper.rb
+++ lib/top_books/scraper.rb
@@ -28,59 +28,5 @@ end
   
   
    
-  # def ask_input(input)
-  #   puts "Input a number"
-  #     input = gets.strip 
-  #   case input 
-  #     when 1 
-  #     grab_title.css(".href")
-  #     when 2 
-  #     puts @@new_array[1]
-  #   end 
-  # end 
 
-    # .collect.with_index {|page,index|
-    #     "#{index + 1}:#{page.text}"}
-         
-    # booklist = site.css(doc).collect.with_index {|page,index|
-    #     "#{index + 1}:#{page.text}"}
-
-    # @@booklist.select do |book|
-          
-    #       @@all << book 
-    #   end  
-  
-  
-  # def get_site 
-  #   site = Nokogiri::HTML(open("https://www.barnesandnoble.com/b/new-releases/_/N-1oyg"))
-  # end 
-  
-  # def get_books
-  #   site = get_site 
-  #       new_site = site.css(".product-shelf-title a")
-  # end 
-  
-  # def create_book_list 
-  #   get_books.collect do |new_site|
-  #       book = Books.new 
-  #       book.title = new_site.children 
-  #       puts book.title
-  #     end 
-  # end
-  #def scraper 
-    # author = get_page(page).css(".product-shelf-author")
-     
-     
-    #   author.collect do |writer|
-    #         @@author << writer.text
-    #   end 
-    # titles.each_with_index do |book,index|
-    #     book = Books.new 
-    #     book.title = titles 
-    #   end    
- 
-      #@@all << "#{index + 1}:#{book}" 
-    #title.each.with_index {|book ,index|puts "#{index+=1}:#{book.text.strip}"}
-      # writers = page.css(".product-shelf-author a").children
-    # puts "#{titles[1]} by #{writers[1]}"
  
