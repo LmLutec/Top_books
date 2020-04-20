c6477bf63ebc4b6e6390c45c9e9ba1158e73e779
Lauren McCoy <lm_lutec@outlook.com>
2020-04-20 16:59:41 -0400

    refactored my create_book_list in scraper.rb

--- lib/top_books/books.rb
+++ lib/top_books/books.rb
@@ -1,10 +1,10 @@
-
 class Books 
+
   attr_accessor :title, :author, :release_date, :genre, :description
   
    @@all = []
   
-  def initialize
+  def initialize(title, author, release_date, genre, description)
     @title = title 
     @author = author 
     @release_date = release_date
@@ -15,15 +15,17 @@ class Books
   
   def self.create_list
     Scraper.new.combine_info if @@all.empty? 
+
     @@all.each_with_index do |book, index|
       if book.title != nil
         puts "#{index + 1}: #{book.title}"
-          end 
       end 
+    end 
+
   end 
   
   def self.all 
-    @@all 
+    @@all
   end 
   
   
--- lib/top_books/scraper.rb
+++ lib/top_books/scraper.rb
@@ -10,13 +10,15 @@ class Scraper
    
   def combine_info 
     self.info_container.collect do |container| 
-        book = Books.new 
-        book.title = container.css(".books-front-meta-title").children[0].text   
-        book.author = container.css(".books-front-meta-authorInfo").children[0].text 
-        book.release_date = container.css(".books-front-meta-debut")[0].text.sub("Debuted:", "")
-        book.genre = container.css(".books-front-meta-genre").text.split("Genre:").pop
-        book.description = container.css(".books-front-meta-short").children[0].text 
-        end 
+         
+        title = container.css(".books-front-meta-title").children[0].text   
+        author = container.css(".books-front-meta-authorInfo").children[0].text 
+        release_date = container.css(".books-front-meta-debut")[0].text.sub("Debuted:", "")
+        genre = container.css(".books-front-meta-genre").text.split("Genre:").pop
+        description = container.css(".books-front-meta-short").children[0].text 
+        
+        Books.new(title, author, release_date, genre, description)  
+      end 
   end
   
 end 
